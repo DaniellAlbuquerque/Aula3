dc5160528d0301f87f7c20c973ec636fb99223d4
DaniellAlbuquerque <danieljca3@gmail.com>
2023-03-12 22:47:35 -0300

    Meu primeiro commit

---
+++ Aula3.cs
@@ -0,0 +1,21 @@
+using System;
+
+internal class NewBaseType
+{
+    static void Main()
+    {
+        int num,num2,res;
+        num=10;
+        num2=30;
+        res=num * num2;
+
+        Console.WriteLine("O resultado é: "+res);
+        
+
+
+    }
+}
+
+
+
+
